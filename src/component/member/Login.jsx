466b10db6957461458491f090c12f42c7498af93
mz01-geunhyung <geunhyung@mz.co.kr>
2022-06-15 18:26:51 +0900

    login page

---
+++ src/component/member/Login.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// import styled from "styled-components";
+// import { useLocation } from "react-router-dom";
+
+
+const Login = () => {
+    const [user, setUser] = useState({email: "", password: ""});
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setUser({...user, [name]: value});
+}
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-lg-5">
+                    <div className="card shadow-lg border-0 rounded-lg mt-5">
+                        <div className="card-header">
+                            <h3 className="text-center font-weight-light my-4">
+                                <span>musicat</span>
+                            </h3>
+                        </div>
+                        <div className="card-body">
+                            <form>
+                                <div className="form-floating mb-3">
+                                    <input className="form-control" value={user.email} name="email" type="email" onChange={handleChange} />
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input className="form-control" value={user.password} name="password" type="password" onChange={handleChange} />
+                                </div>
+                                <div className="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                    <button className="btn btn-primary" type="submit">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+    
+            </div>
+        </div>
+    );
+};
+
+export default Login;
