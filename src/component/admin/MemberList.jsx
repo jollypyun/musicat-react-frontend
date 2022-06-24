2eee9e20e8f8f95e175f45ea51c86c69a086db0d
mz01-geunhyung <geunhyung@mz.co.kr>
2022-06-24 12:09:24 +0900

    table

--- src/component/admin/MemberList.jsx
+++ src/component/admin/MemberList.jsx
@@ -1,11 +1,36 @@
-import MemberListForm from "./MemberListForm";
-import { useMemo } from "react";
+import { useState } from "react";
 
-const columns = useMemo(
-    () => [
-        {
-            accessor: 'NO',
-            Header: 'NO'
-        }
-    ],[]
-);
+const MemberList = () => {
+    const [datas, setDatas] = useState([]);
+    return(
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>NO</th>
+                        <th>ID</th>
+                        <th>등급</th>
+                        <th>가입 날짜</th>
+                        <th>방문 횟수</th>
+                        <th>비고</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {datas?.map((row) => {
+                        <tr>
+                            <td>{row.no}</td>
+                            <td>{row.email}</td>
+                            <td>{row.no}</td>
+                            <td>{row.no}</td>
+                            <td>{row.no}</td>
+                            <td>{row.no}</td>
+                            <td>{row.no}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default MemberList;
